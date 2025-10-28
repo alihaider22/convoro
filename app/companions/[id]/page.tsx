2147fc95f3d41d68f7bb8ae64e288f51ec083374
Ali Haider <alihaider@foomotion.io>
2025-10-28 09:46:59 +0500

    fix: params value issue

--- app/companions/[id]/page.tsx
+++ app/companions/[id]/page.tsx
@@ -1,5 +1,5 @@
-const page = ({ params }: { params: { id: string } }) => {
-  const { id } = params;
+const page = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params;
   return <div>companion detail page with id: {id}</div>;
 };
 
