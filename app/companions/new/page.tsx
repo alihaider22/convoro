16fcb7bac9105e7e4c42e27f6d4b1854da9abb02
Ali Haider <alihaider@foomotion.io>
2025-11-07 18:37:46 +0500

    feat: add authentication check to CompanionForm page to ensure user is signed in

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,6 +1,13 @@
 import CompanionForm from "@/components/CompanionForm";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const page = () => {
+const page = async () => {
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/sign-in?redirect_url=/companions/new");
+  }
+
   return <CompanionForm />;
 };
 
