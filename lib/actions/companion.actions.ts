e42df85fe37c4a7230fc2d3b64ffd7a03be950f8
Ali Haider <alihaider@foomotion.io>
2025-11-10 17:55:39 +0500

    feat: add getCompanions action to retrieve a paginated list of companions with optional filtering by topic and subject

--- lib/actions/companion.actions.ts
+++ lib/actions/companion.actions.ts
@@ -19,3 +19,35 @@ export const createCompanion = async (formData: CompanionFormData) => {
 
   return data[0];
 };
+
+export const getCompanions = async ({
+  limit = 10,
+  page = 1,
+  topic = "",
+  subject = "",
+}: {
+  limit?: number;
+  page?: number;
+  topic?: string;
+  subject?: string;
+}) => {
+  const supabase = createSupabaseClient();
+
+  let query = supabase.from("companions").select();
+
+  if (topic && subject) {
+    query = query.ilike("topic", `%${topic}%`).ilike("subject", `%${subject}%`);
+  } else if (topic) {
+    query = query.ilike("topic", `%${topic}%`);
+  } else if (subject) {
+    query = query.ilike("subject", `%${subject}%`);
+  }
+
+  query = query.range((page - 1) * limit, page * limit - 1);
+
+  const { data, error } = await query;
+  if (error || !data) {
+    throw new Error(error.message || "Failed to get companions");
+  }
+  return data;
+};
